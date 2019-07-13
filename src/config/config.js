3083888b93d490af69e40360e470427296e489b5
akhilomar <akhilomar555@gmail.com>
2019-07-13 15:27:16 +0530

    To Do List

---
+++ src/config/config.js
@@ -0,0 +1,9 @@
+  export const DB_CONFIG = {
+    apiKey: "AIzaSyCa2pPGDKclAUzhbAO0v4-DCzsamQOzsxo",
+    authDomain: "react-apps-3786e.firebaseapp.com",
+    databaseURL: "https://react-apps-3786e.firebaseio.com",
+    projectId: "react-apps-3786e",
+    storageBucket: "",
+    messagingSenderId: "745631729272",
+    appId: "1:745631729272:web:f93faffc252fe745"
+  };
